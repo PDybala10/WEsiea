30a3dccdca902d708818e05293c62ae6b669cff4
Igor <igorndesigne@gmail.com>
2020-03-18 16:51:42 +0100

    fragment_home_offline

--- app/src/main/java/com/example/wesiea/ui/home/HomeFragmentOffline.java
+++ app/src/main/java/com/example/wesiea/ui/home/HomeFragmentOffline.java
@@ -14,15 +14,15 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.wesiea.R;
 
-public class HomeFragment extends Fragment {
+public class HomeFragmentOffline extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private HomeViewModelOffline homeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+                ViewModelProviders.of(this).get(HomeViewModelOffline.class);
+        View root = inflater.inflate(R.layout.fragment_home_offline, container, false);
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
